45b54a7ab91e31f96a4d8a6c6ae048db89053d50
architnangla7 <136116152+architnangla7@users.noreply.github.com>
2023-06-10 23:47:39 +0530

    Add files via upload

---
+++ lecture4.cpp
@@ -0,0 +1,667 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /*
+    1 2 3
+    1 2 3
+    1 2 3
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        int j = 1;
+        while(j<=n)
+        {
+            cout << j << " ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+
+
+    /*
+    3 2 1
+    3 2 1
+    3 2 1
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        int j = 1;
+        while(j<=n)
+        {
+            cout << n - j + 1 << " ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+
+
+    /*
+    1 2 3
+    4 5 6
+    7 8 9
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int count =1; 
+    int i = 1;
+    while(i<=n)
+    {
+        int j = 1;
+        while(j<=n)
+        {
+            cout << count << " ";
+            count = count + 1;
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+
+
+    /*
+    *
+    * *
+    * * * 
+    * * * *
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        int j = 1;
+        while(j<=i)
+        {
+            cout << "*"
+                 << " ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+    
+    
+    /*
+    1
+    2 2
+    3 3 3
+    4 4 4 4
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        int j = 1;
+        while(j<=i)
+        {
+            cout << i << " ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+    
+    
+    /*
+    1
+    2 3
+    4 5 6
+    7 8 9 10
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int count = 1;
+    int i = 1;
+    while(i<=n)
+    {
+        int j = 1;
+        while(j<=i)
+        {
+            cout << count << " ";
+            count = count + 1;
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+   
+
+    /*
+    1
+    2 3
+    3 4 5
+    4 5 6 7
+    5 6 7 8 9
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        int j = 1;
+        int value = i;
+        while(j<=i)
+        {
+            cout << value << " ";
+            value = value + 1;
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+   /*
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= i)
+        {
+            cout << (i + j - 1) << " ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+
+
+    /*
+    1
+    2 1
+    3 2 1 
+    4 3 2 1
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        int j = 1;
+        while(j<=i)
+        {
+            cout << (i - j + 1) << " ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+
+
+    /*
+    A A A 
+    B B B
+    C C C
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        int j = 1;
+        while(j<=n)
+        {
+            char ch = 'A' + i - 1;
+            cout << ch << " ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+    
+
+    /*
+    A B C
+    A B C
+    A B C 
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i=1;
+    while(i<=n)
+    {
+        int j = 1;
+        while(j<=n)
+        {
+            char ch = 'A' + j - 1;
+            cout << ch << " ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+   
+    
+
+    /*
+    A B C D 
+    E F G H 
+    I J K L 
+    M N O P
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    char ch = 'A';
+    while(i<=n)
+    {
+        int j = 1;
+        while(j<=n)
+        {
+            cout << ch << " ";
+            ch = ch + 1;
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+
+
+    /*
+    A B C D 
+    B C D E 
+    C D E F 
+    D E F G
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        int j = 1;
+        while(j<=n)
+        {
+            char ch = 'A' + i + j - 2;
+            cout << ch << " ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+
+
+    /*
+    A
+    B B
+    C C C 
+    D D D D
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= i)
+        {
+            char ch = 'A' + i - 1;
+            cout << ch << " ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+
+
+    /*
+    A
+    B C
+    D E F
+    G H I J 
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    char ch = 'A';
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= i)
+        {
+            cout << ch << " ";
+            ch = ch + 1;
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+
+
+    /*
+    A 
+    B C 
+    C D E 
+    D E F G
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        int j = 1;
+        while(j<=i)
+        {
+            char ch = 'A' + i + j - 2;
+            cout << ch << " ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+
+
+    /*
+    D
+    C D
+    B C D
+    A B C D 
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        char start = 'A' + n - i;
+        int j = 1;
+        while(j<=i)
+        {
+            cout << start << " ";
+            start = start + 1;
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+
+ 
+    /*
+         *
+       * *
+     * * *
+    * * * *
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        int space = n - i;
+        while(space)
+        {
+            cout << " "
+                 << " ";
+            space = space - 1;
+        }
+        int j = 1;
+        while(j<=i)
+        {
+            cout << "*"
+                 << " ";
+            j = j + 1;
+        }
+        cout << endl;
+        i=i+1;
+    }
+    */
+
+
+    /*
+    * * * *
+    * * *
+    * *
+    *
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        int j = 1;
+        while(j<=n-i+1)
+        {
+            cout << "*"
+                 << " ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+
+
+   /*
+    * * * * *
+      * * * *
+        * * *
+          * *
+            *
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        int space = i - 1;
+        while(space)
+        {
+            cout << " "
+                 << " ";
+            space = space - 1;
+        }
+        int stars = n - i + 1;
+        while(stars)
+        {
+            cout << "*"
+                 << " ";
+            stars = stars - 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+
+
+    /*
+    1 1 1 1
+      2 2 2
+        3 3
+          4
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        int space = i - 1;
+        while(space)
+        {
+            cout << " "
+                 << " ";
+            space = space - 1;
+        }
+        int j = 1;
+        while(j<=n-i+1)
+        {
+            cout << i << " ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+
+
+    /*
+          1
+        2 2
+      3 3 3
+    4 4 4 4
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        int space = n - i;
+        while(space)
+        {
+            cout << " "
+                 << " ";
+            space = space - 1;
+        }
+        int j = 1;
+        int value = i;
+        while(j<=i)
+        {
+            cout << value << " ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+
+
+   /*
+   1 2 3 4
+     2 3 4
+       3 4
+         4
+   int n;
+   cout << "Enter the value of n:->" << endl;
+   cin >> n;
+   int i = 1;
+   while(i<=n)
+   {
+       int space = i - 1;
+       while(space)
+       {
+           cout << " "
+                << " ";
+           space = space - 1;
+       }
+       int j = 1;
+       int value = i;
+       while(j<=n-i+1)
+       {
+           cout << value << " ";
+           value = value + 1;
+           j = j + 1;
+       }
+       cout << endl;
+       i = i + 1;
+   }
+   */
+
+
+    /*
+                1
+              1 2 1
+            1 2 3 2 1
+          1 2 3 4 3 2 1
+        1 2 3 4 5 4 3 2 1
+      1 2 3 4 5 6 5 4 3 2 1
+    int n;
+    cout<<"Enter the value of n:->"<<endl;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        int space = n - i;
+        while(space)
+        {
+            cout<<" "<<" ";
+            space = space - 1;
+        }
+        int j = 1;
+        while(j<=i)
+        {
+            cout << j << " ";
+            j = j + 1;
+        }
+        int start = i - 1;
+        while(start)
+        {
+            cout << start << " ";
+            start = start - 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+
+
+    /*
+    1 2 3 4 5 5 4 3 2 1
+    1 2 3 4 * * 4 3 2 1
+    1 2 3 * * * * 3 2 1
+    1 2 * * * * * * 2 1
+    1 * * * * * * * * 1
+    int n;
+    cout << "Enter the value of n:->" << endl;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        int j = 1;
+        int value = 1;
+        while(j<=n-i+1)
+        {
+            cout << value << " ";
+            value = value + 1;
+            j = j + 1;
+        }
+        int star = i - 1;
+        while(star)
+        {
+            cout << "*"
+                 << " ";
+            star = star - 1;
+        }
+        int stars = i - 1;
+        while(stars)
+        {
+            cout << "*"
+                 << " ";
+            stars = stars - 1;
+        }
+        int values = n - i + 1;
+        while(values)
+        {
+            cout << values << " ";
+            values = values - 1;
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+}
